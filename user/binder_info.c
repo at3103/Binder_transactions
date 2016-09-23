80d4284b63f89dd47bf435949c94aa95307b94ca
jangsutsr <jangsutsr@gmail.com>
2016-09-22 21:35:48 -0400

    Bug fixes and refactoring on test file.

--- user/binder_info.c
+++ user/binder_info.c
@@ -5,49 +5,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
-	/*Stub implementation*/
 	pid_t pid;
-	char *command = NULL;
-	size_t *size = NULL;
-	struct binder_stats stats;
-	void *buf;
-	struct binder_peer *peer;
-	size_t l;
+	size_t size;
+	void *buf = NULL;
+	struct binder_stats *stats = (struct binder_stats *)NULL;
+	struct binder_peer *peer = (struct binder_peer *)NULL;
+	int is_invalid = 0;
+	long count, i;
 
-	buf = malloc(4096);
-	l = sizeof(struct binder_peer);
-	peer = (struct binder_peer *)buf;
-	pid = (pid_t) atoi(argv[0]);
+	if (argc != 3) {
+		is_invalid = 1;
+		goto err_handle;
+	}
+	pid = (pid_t)atoi(argv[2]);
+	if ((int)pid == 0) {
+		is_invalid = 1;
+		goto err_handle;
+	}
 
-	strcpy(command, argv[1]);
-
-	if (strcmp(command, "start")) {
-		if (syscall(244, pid, 1) == -1) {
-			fprintf(stderr, "Error processing pid %u: %s\n",
-				pid, strerror(errno));
-		}
-	} else if (strcmp(command, "print")) {
-		if (syscall(245, pid, stats, buf, size) == -1) {
-			fprintf(stderr, "Error processing pid %u: %s\n",
-				pid, strerror(errno));
-		}
+	if (strcmp(argv[1], "start") == 0) {
+		if (syscall(244, pid, 1) != 0)
+			goto err_handle;
+	} else if (strcmp(argv[1], "print") == 0) {
+		count = syscall(245, pid, stats, buf, &size);
+		if (count < 0L)
+			goto err_handle;
+		peer = (struct binder_peer *)buf;
+		/*
 		printf("%s (%u):\t%u bytes\t%u transactions\n",
-			stats.comm, pid, stats.bytes, stats.nr_trans);
-		for (; peer != NULL; peer += l) {
-			printf("\t\t%s\t%u\t%u\n", peer->comm,
-				peer->pid, peer->uid);
-		}
-	} else if (strcmp(command, "stop")) {
-		if (syscall(244, pid, 0) == -1) {
-				fprintf(stderr, "Error processing pid %u: %s\n",
-					pid, strerror(errno));
-			}
-	} else
-		printf("Error: Invalid argument\n");
+		       stats->comm, pid, stats->bytes, stats->nr_trans);
+		*/
+		for (i = 0L; i < count; i++)
+			printf("\t\t%s\t%u\t%u\n", peer[i].comm,
+			       peer[i].pid, peer[i].uid);
+	} else if (strcmp(argv[1], "stop") == 0) {
+		if (syscall(244, pid, 0) != 0)
+			goto err_handle;
+	} else {
+		is_invalid = 1;
+		goto err_handle;
+	}
+	goto terminate;
 
+err_handle:
+	if (is_invalid)
+		fprintf(stderr, "Error: Invalid argument\n");
+	else
+                fprintf(stderr, "Error processing pid %u: %s\n",
+			pid, strerror(errno));
+terminate:
 	free(buf);
+	free(stats);
 	return 0;
 }
