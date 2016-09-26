363a92322e01971124994d2c3799c0b125eb65df
Ashwin Karthik Tamilselvan <w4118@w4118>
2016-09-26 01:55:19 -0400

    Removed goto statements

--- user/binder_info.c
+++ user/binder_info.c
@@ -19,46 +19,46 @@ int main(int argc, char **argv)
 
 	if (argc != 3) {
 		is_invalid = 1;
-		goto err_handle;
+	/*	goto err_handle;*/
 	}
-	pid = (pid_t)atoi(argv[2]);
+	pid = (pid_t)atoi(argv[0]);
 	if ((int)pid == 0) {
 		is_invalid = 1;
-		goto err_handle;
+/*		goto err_handle;*/
 	}
 
 	if (strcmp(argv[1], "start") == 0) {
 		if (syscall(244, pid, 1) != 0)
-			goto err_handle;
+			is_invalid = 2;
+/*			goto err_handle;*/
 	} else if (strcmp(argv[1], "print") == 0) {
 		count = syscall(245, pid, stats, buf, &size);
 		if (count < 0L)
-			goto err_handle;
+			is_invalid = 2;
+/*			goto err_handle;*/
 		peer = (struct binder_peer *)buf;
-		/*
 		printf("%s (%u):\t%u bytes\t%u transactions\n",
 		       stats->comm, pid, stats->bytes, stats->nr_trans);
-		*/
 		for (i = 0L; i < count; i++)
 			printf("\t\t%s\t%u\t%u\n", peer[i].comm,
 			       peer[i].pid, peer[i].uid);
 	} else if (strcmp(argv[1], "stop") == 0) {
 		if (syscall(244, pid, 0) != 0)
-			goto err_handle;
+			is_invalid = 2;
+/*			goto err_handle;*/
 	} else {
 		is_invalid = 1;
-		goto err_handle;
+/*		goto err_handle;*/
 	}
-	goto terminate;
 
-err_handle:
-	if (is_invalid)
-		fprintf(stderr, "Error: Invalid argument\n");
-	else
-                fprintf(stderr, "Error processing pid %u: %s\n",
-			pid, strerror(errno));
-terminate:
-	free(buf);
-	free(stats);
-	return 0;
+/*err_handle:*/
+if (is_invalid == 1)
+	fprintf(stderr, "Error: Invalid argument\n");
+else if (is_invalid == 2)
+	fprintf(stderr, "Error processing pid %u: %s\n",
+	pid, strerror(errno));
+/*terminate:*/
+free(buf);
+free(stats);
+return 0;
 }
